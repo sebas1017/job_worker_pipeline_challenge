b237969f66e58b5bce990f516774f574deb9c8e9
sebas1017 <henaoerazosebastian995@gmail.com>
2022-05-17 10:29:00 -0500

    ajustes en service

--- job_worker.py
+++ job_worker.py
@@ -5,13 +5,26 @@ import uvicorn
 import os
 server = FastAPI()
 
+environment = "PROD" #CA:MBIAR A DEV SI DESEA LOCALHOST
+if environment == "PROD":
+    manager_host='https://manager-pipeline-challenge.herokuapp.com'
+    manager_port=443
+    api_url="https://api-challenge-pipeline.herokuapp.com"
+    api_port=443
+else:
+    manager_host='0.0.0.0'
+    manager_port=8220
+    api_url="http://localhost:8000"
+
+
+
 @server.on_event('startup')
 async def setup():
     worker = await EasyJobsWorker.create(
         server,
         server_secret='abcd1234',
-        manager_host='https://manager-pipeline-challenge.herokuapp.com',
-        manager_port=443,
+        manager_host=manager_host, # cambiar esto por 0.0.0.0 en caso de ejecutar localmente el manager
+        manager_port=manager_port, # cambiar este puerto a 8220 en caso de ejecutar localmente el manager
         manager_secret='abcd1234',
         jobs_queue='ETL',
         max_tasks_per_worker=5
@@ -35,12 +48,13 @@ async def setup():
            r = await client.get(url, timeout=timeout)
         return {'message': "insert_delegaciones finalizado correctamente"}
 
-    @worker.task(schedule=every_minute,   default_args=default_args)
-    async def extract(url: str):
-        url = "http://localhost:8000/api/v1/insert_vehicles"
+    @worker.task(schedule=every_minute)
+    async def extract():
+        url = f"{api_url}/api/v1/insert_vehicles"
+        print("entreeeeeeeeee")
         print(f"PROCESO DE INSERCION INICIADO...")
         await get_data(url)
-        url = "http://localhost:8000/api/v1/insert_delegaciones"
+        url = f"{api_url}/api/v1/insert_delegaciones"
         await insert_delegaciones(url)
         print(f"PROCESO DE INSERCION FINALIZADO...")
         return {'data': "INSERCION DE DATOS FINALIZADA CORRECTAMENTE"}
